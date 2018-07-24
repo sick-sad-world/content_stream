db4d6337611bef9d9c1eb54e454d810d6dfdf1f6
ikoptel <ikoptel@softserveinc.com>
2018-07-24 13:25:57 +0300

    Access and color for inner SVG dots on map for navigation

--- assets/src/index.js
+++ assets/src/index.js
@@ -3,3 +3,7 @@ import '../css/theme.css';
 import '../css/index.css';
 
 fontLoadingAnalyzer(['300 1em Lato', 'italic 300 1em Lato', '1em Lato', '700 1em Lato', 'italic 1em Lato', 'italic 700 1em Lato']);
+
+document.getElementById('map').addEventListener('load', function () {
+  console.log(this.getSVGDocument());
+});
